01b8f9ec1b1fcb5929cda30a8a5931ea612c5a2c
Aditya Raj <76516486+projecthub000@users.noreply.github.com>
2021-02-09 10:38:59 +0530

    Add files via upload

---
+++ domify.js
@@ -0,0 +1,25 @@
+(function() {
+    let inputItem = document.querySelector('#input');
+    inputItem.onkeyup = function(e) {
+      if (e.keyCode === 13) {
+        if (inputItem.value) {
+          addItem(inputItem);
+        }
+        else{
+          alert("You must write something!")
+        }
+      }
+    };
+  
+    function addItem(item) {
+      let ul = document.querySelector('#list');
+      let li = document.createElement('li');
+      let textNode = document.createTextNode(inputItem.value);
+      li.appendChild(textNode);
+      ul.appendChild(li);
+      inputItem.value = '';
+      li.onclick = function(e) {
+        e.target.remove();
+      };
+    }
+  })();
